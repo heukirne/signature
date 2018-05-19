e8c52cf9c619028d300327baf7676dc538b6ced3
Henrique Dias <heukirne@gmail.com>
2018-05-19 15:22:55 -0300

    reading the input

--- antivirus.c
+++ antivirus.c
@@ -1,21 +1,27 @@
+// Inclue bibliotecas basicas
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
 
+// Comeca funcao principal
 int main(int argc, char* argv[]) {
 	
-	if (argc < 4) { 
-		printf("Usage: host port dir [dir]\n");
+	// Garante que haja somente um parametro 
+	if (argc != 2) { 
+		printf("Usage: antivirus signature < file_list\n");
 		return 0;
 	}
-	int port;
-	int r = sscanf(argv[2], "%d", &port);
-	if(!r){
-		printf("Port must be integer\n");
-		return 2;
-	}
+
+    char *line = NULL;
+    size_t size;
+	// 	Le cada linha da entrada padrao
+    while (getline(&line, &size, stdin) != -1)
+    {
+        printf("%s\n", line);
+    }
 
 	return 0;
 }
