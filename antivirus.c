2dce2914885a59607e93ee7be2722e45e40eed64
Henrique Dias <heukirne@gmail.com>
2018-05-20 09:25:16 -0300

    checking signature in normal files

--- antivirus.c
+++ antivirus.c
@@ -12,50 +12,74 @@ const unsigned short gzip_byte2 = 0x8b;
 // Comeca funcao principal
 int main(int argc, char* argv[]) {
 	
+	// Variavel para guardar assinatura do virus
+	char *signature = NULL;
+
 	// Garante que haja somente um parametro 
 	if (argc != 2) { 
 		printf("Usage: antivirus signature < file_list\n");
 		return 0;
+	} else {
+		signature = argv[1];
 	}
 
 	// 	Le cada linha da entrada padrao
-    char *line = NULL;
+    char *arquivo = NULL;
     size_t size;
-    while (getline(&line, &size, stdin) != -1)
+    while (getline(&arquivo, &size, stdin) != -1)
     {
+    	// Variavel para guardar se arquivo tem assinatura do virus
+    	int found = 0;
+
     	// Remove quebra de linha do nome do arquivo
+    	// Nao tinha a funcao memmem no meu gcc
     	char *temp = NULL;
-    	if ((temp = strstr(line, "\n")) != NULL) {
-    		int len = strlen(line);
-    		line[len-1] = '\0';
+    	if ((temp = strstr(arquivo, "\n")) != NULL) {
+    		int len = strlen(arquivo);
+    		arquivo[len-1] = '\0';
     	}
 
     	// Verifica se o arquivo existe
-		if( access( line, F_OK ) != -1 ) {
+		if( access( arquivo, F_OK ) != -1 ) {
 
 			FILE *file;
 			unsigned char file_byte1[1];
 			unsigned char file_byte2[1];
 
-			file = fopen(line, "r");
+			file = fopen(arquivo, "r");
 			fread(&file_byte1, 1, 1, file);
+			// TODO: Ainda nao esta funcionando a comparacao de 2 bytes 
 			//fread(&file_byte2, 2, 1, file);
 
+			// Verifica se o arquivo esta compactado
 			if(memcmp(file_byte1, &gzip_byte1, 1) == 0) {
-				printf("Arquivo %s compactado\n", line);
+				printf("Arquivo %s compactado\n", arquivo);
 			} else {
-				printf("Arquivo %s nao compactado\n", line);
+				printf("Arquivo %s nao compactado\n", arquivo);
+
+				// 	Le cada linha do arquivos
+			    char *linha = NULL;
+			    size_t tamanho;
+			    while (getline(&linha, &tamanho, file) != -1)
+			    {
+			    	if ((temp = strstr(linha, signature)) != NULL) {
+			    		found = 1;
+			    	}
+			    }
+
 			}
 
+			if (found == 1) {
+				fprintf( stderr, "Arquivo %s contem a assinatura do virus\n", arquivo);
+			} else {
+				fprintf( stdout, "Arquivo %s não contem a assinatura do virus\n", arquivo);
+			}
 
 		} else { //Se o arquivo não existe
-			printf("Arquivo %s nao encontrado!\n", line);
+			printf("Arquivo %s nao encontrado!\n", arquivo);
 		}
 
     }
-
-    fprintf( stderr, "Arquivo %s contem a assinatura do virus\n", "file1.txt");
-    fprintf( stdout, "Arquivo %s não contem a assinatura do virus\n", "file2.txt");
 
 	return 0;
 }
